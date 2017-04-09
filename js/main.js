3349ea5fe30b94cb7ab5c962b9a058bf9607dedf
Maarten Dings <maarten.dings@cxcompany.com>
2017-04-09 20:14:14 +0200

    index on gh-pages: 4b40312 updates towards v1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,69 @@
-const $ = require('jquery')
+window.noZensmooth = true
 
-;(function($){
+const imagesLoaded = require('imagesloaded')
+const Flickity = require('flickity')
+const zenscroll = require('zenscroll')
 
-    $('.menu__name').on('click', function() {
+const MENU_ACTIVE_CLASS = 'is-menu-active'
+const gallery = document.querySelector('[data-carousel]')
+const burger = document.querySelector('[data-hamburger]')
+const menu = document.querySelectorAll('.nav__main a')
+const mq = window.matchMedia("(max-width: 768px)");
+let flkty
 
-        $(this).next().slideToggle(100)
+const setupFlickity = () => {
+
+    if(mq.matches) {
+
+        imagesLoaded(gallery, () => {
+
+            flkty = new Flickity(gallery, {
+                adaptiveHeight: true
+            });
+        })
+
+    } else {
+
+        if (flkty) {
+
+            flkty.destroy()
+        }
+    }
+
+}
+
+mq.addListener(setupFlickity)
+setupFlickity()
+
+
+burger.addEventListener('click', e => {
+
+    document.body.classList.toggle(MENU_ACTIVE_CLASS)
+    burger.classList.toggle('is-active')
+})
+
+;[].forEach.call(menu, link => {
+
+    link.addEventListener('click', () => {
+
+        if (document.body.classList.contains(MENU_ACTIVE_CLASS)) {
+
+            document.body.classList.remove(MENU_ACTIVE_CLASS)
+        }
+
+        const toScrollSection = link.getAttribute('href').replace(/\#/, '')
+        zenscroll.to(document.getElementById(toScrollSection))
+        return false;
     })
-})($)
+})
+
+const toTopLinks = document.querySelectorAll('[data-totop]')
+
+;[].forEach.call(toTopLinks, (link) => {
+
+    link.addEventListener('click', () => {
+
+        zenscroll.toY(0)
+        return false;
+    })
+})
